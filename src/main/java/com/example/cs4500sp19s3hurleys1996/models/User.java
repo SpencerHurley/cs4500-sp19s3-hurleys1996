bef0d5dc85384d6d0e3fa0ab7e30a7924be8cd7b
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2019-01-23 15:37:15 -0500

    Added User class

---
+++ src/main/java/com/example/cs4500sp19s3hurleys1996/models/User.java
@@ -0,0 +1,34 @@
+package com.example.cs4500sp19s3hurleys1996.models;
+
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+ 	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
